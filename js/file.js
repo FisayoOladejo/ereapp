c92df82a170442836aa46cefdc1caba1b78ec8cc
Fisayo Oladejo <108827240+FisayoOladejo@users.noreply.github.com>
2023-03-05 18:05:38 +0100

    Project js files

---
+++ js/file.js
@@ -0,0 +1,34 @@
+let subProfile = document.getElementById("subProfile");
+
+function toggleProfile(){
+    subProfile.classList.toggle("open-profile")
+}
+
+
+let subProfile1 = document.getElementById("subProfile1");
+
+function toggleProfile1(){
+    subProfile1.classList.toggle("open-profile")
+}
+
+
+
+let subProfilesignUp = document.getElementById("subProfilesignUp");
+
+function toggleProfilesignUp(){
+    subProfilesignUp.classList.toggle("open-profile")
+} 
+
+
+
+let subProfileContact = document.getElementById("subProfileContact");
+
+function toggleProfileContact(){
+    subProfileContact.classList.toggle("open-profile")
+} 
+
+
+
+
+
+
